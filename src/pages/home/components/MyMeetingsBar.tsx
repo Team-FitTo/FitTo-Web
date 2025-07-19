2b532e16354e4b8d31937c4d56ba7ca812ad0d84
JavaNo0b <98101954+JavaNo0b@users.noreply.github.com>
2025-07-19 18:12:55 +0900

    feat: scrollbar 삭제

--- src/pages/home/components/MyMeetingsBar.tsx
+++ src/pages/home/components/MyMeetingsBar.tsx
@@ -10,7 +10,7 @@ interface MyMeetingsBarProps {
 }
 
 const MyMeetingsBar: React.FC<MyMeetingsBarProps> = ({ meetings, onAddMeeting, onMeetingClick }) => (
-  <div className="w-full max-w-[400px] mx-auto flex gap-1 justify-start items-center overflow-x-auto whitespace-nowrap scrollbar-hide">
+  <div className="w-full pl-4 max-w-[400px] flex gap-1 justify-start items-center overflow-x-auto whitespace-nowrap scrollbar-hide">
     {/* vector(추가) 버튼 */}
     <button className="mr-1 flex flex-col items-center focus:outline-none" onClick={onAddMeeting}>
       <span className="w-[55px] h-[55px] flex items-center justify-center rounded-full">
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -24,15 +24,17 @@ const HomePage: React.FC = () => {
   };
 
   return (
-    <div className="min-h-screen bg-brand-bg flex flex-col items-center pt-[68px] px-[35px]">
+    <div className="min-h-screen bg-brand-bg flex flex-col items-center pt-[68px]">
       {/* 상단 내 미팅 버튼 영역 */}
       <MyMeetingsBar meetings={myMeetings} onAddMeeting={() => {}} onMeetingClick={handleMeetingClick} />
       {/* 카드 컨테이너 */}
-      <div className="w-full bg-white rounded-t-[54px] px-6 pt-6 pb-6 mt-[40px] flex-1 flex flex-col shadow-lg">
-        {/* 필터 버튼 */}
-        <FilterButton onClick={() => {}} />
-        {/* 내가 들어가있지 않은 미팅 리스트 */}
-        <MeetingCardList meetings={otherMeetings} />
+      <div className="w-full px-[35px] flex-1 flex flex-col">
+        <div className="w-full bg-white rounded-t-[54px] px-6 pt-6 pb-[60px] mt-[40px] flex-1 flex flex-col shadow-lg">
+          {/* 필터 버튼 */}
+          <FilterButton onClick={() => {}} />
+          {/* 전체 미팅 리스트 */}
+          <MeetingCardList meetings={otherMeetings} />
+        </div>
       </div>
     </div>
   );
